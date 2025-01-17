041eaa0edfd8932430cb87ea3148d2daaac8e8ae
JellyO1 <danivangelis@hotmail.com>
2025-01-17 16:28:08 +0000

    Remove read/write ipcMain listeners when the browserWindow closes.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
 import { BackendModule } from "i18next";
 
+export declare const readFileRequest = "ReadFile-Request";
+export declare const writeFileRequest = "WriteFile-Request";
+export declare const readFileResponse = "ReadFile-Response";
+export declare const changeLanguageRequest = "ChangeLanguage-Request";
 export function mainBindings(ipcMain: Electron.IpcMain, browserWindow: Electron.BrowserWindow, fs: any): any;
 export function clearMainBindings(ipcMain: Electron.IpcMain): any;
 export function preloadBindings(ipcRenderer: Electron.IpcRenderer, process: NodeJS.Process): any;
